cc65820c2e763f70da5352a1e70a6961e2c2996b
baeksnip <76125829+baeksnip@users.noreply.github.com>
2023-03-03 12:47:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
+$fileList = glob('./192.168.100.*');
+foreach($fileList as $filename){
+        if(is_file($filename)){
+                echo '<b>', $filename, ':</b>';
+                $lines = fopen($filename,'r');
+                while ($line = fgets($lines)) {
+                        echo($line);
+                }
+        fclose($fh);
+        echo '<br>';
+    }
+}
+
 ?>

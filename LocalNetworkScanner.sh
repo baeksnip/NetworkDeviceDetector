501ec9a2822f77b8a8667b59366e8efc79a6c86d
baeksnip <76125829+baeksnip@users.noreply.github.com>
2023-03-13 10:59:51 +0100

    Update LocalNetworkScanner.sh

--- LocalNetworkScanner.sh
+++ LocalNetworkScanner.sh
@@ -26,7 +26,6 @@ done
 #Escaneo de puertos si el fichero de la ip esta vacio
 for fichero_ip in `ls $ruta_ficheros`; do
         if ! [ -s $ruta_ficheros$fichero_ip ]; then
-                printf $fichero_ip"\n"
                 for port in $(seq 1 65535); do
                         timeout 0.1 bash -c "echo '' > /dev/tcp/$fichero_ip/$port" 2>/dev/null && echo -n $port"," >> $ruta_ficheros$fichero_ip
                 done; wait

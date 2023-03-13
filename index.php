7eb232a8bcdb5b1a7d4252889d922208f490138c
baeksnip <76125829+baeksnip@users.noreply.github.com>
2023-03-13 10:40:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
 <?php
+$fileList = glob('./ips/192.168.*');
+natsort($fileList);
 
-$fileList = glob('./192.168.100.*');
+print ("<table border=1>");
 foreach($fileList as $filename){
         if(is_file($filename)){
-                echo '<b>', $filename, ':</b>';
+                $ip_array = explode ("./ips/", $filename);
+                $ip = $ip_array[1];
+                print ("<tr>");
+                print ("<td><b>".$ip."</b></td>");
+
                 $lines = fopen($filename,'r');
                 while ($line = fgets($lines)) {
-                        echo($line);
+                        print ("<td>".$line."</td>");
                 }
-        fclose($lines);
-        echo '<br>';
-    }
-}
 
+                $ruta_nombres = "./nombres/".$ip;
+                $nombres = fopen($ruta_nombres,"r");
+                print ("<td><font color=blue>".fgets($nombres)."</font></td>");
+                fclose($ruta_nombres);
+                fclose($lines);
+
+                $ruta_arp = "./arp/".$ip;
+                $arp = fopen($ruta_arp,"r");
+                print ("<td><font color=red>".fgets($arp)."</font></td>");
+                fclose($ruta_arp);
+
+                fclose($lines);
+                print ("</tr>");
+        }
+}
+print ("</table>");
 ?>

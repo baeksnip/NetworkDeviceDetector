ac57950b32c5bb61e2c61e2807a09a31aefe7cfe
baeksnip <76125829+baeksnip@users.noreply.github.com>
2023-03-13 10:53:03 +0100

    Update LocalNetworkScanner.php

--- LocalNetworkScanner.php
+++ LocalNetworkScanner.php
@@ -8,23 +8,23 @@ foreach($fileList as $filename){
                 $ip_array = explode ("./ips/", $filename);
                 $ip = $ip_array[1];
                 print ("<tr>");
+
+                $ruta_nombres = "./nombres/".$ip;
+                $nombres = fopen($ruta_nombres,"r");
+                print ("<td><font color=blue>".fgets($nombres)."</font></td>");
+                fclose($ruta_nombres);
+
+                $ruta_arp = "./arp/".$ip;
+                $arp = fopen($ruta_arp,"r");
+                print ("<td><font color=red>".fgets($arp)."</font></td>");
+                fclose($ruta_arp);
+
                 print ("<td><b>".$ip."</b></td>");
 
                 $lines = fopen($filename,'r');
                 while ($line = fgets($lines)) {
                         print ("<td>".$line."</td>");
                 }
-
-                $ruta_nombres = "./nombres/".$ip;
-                $nombres = fopen($ruta_nombres,"r");
-                print ("<td><font color=blue>".fgets($nombres)."</font></td>");
-                fclose($ruta_nombres);
-                fclose($lines);
-
-                $ruta_arp = "./arp/".$ip;
-                $arp = fopen($ruta_arp,"r");
-                print ("<td><font color=red>".fgets($arp)."</font></td>");
-                fclose($ruta_arp);
 
                 fclose($lines);
                 print ("</tr>");

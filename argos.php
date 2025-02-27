89499eb9cc0c4db58b94de2f326be5c5b2066aee
baeksnip <76125829+baeksnip@users.noreply.github.com>
2025-02-27 10:04:38 +0100

    Update argos.php

--- argos.php
+++ argos.php
@@ -1,26 +1,27 @@
 <?php
-function check($ip,$puerto){
-    if($puerto <> "0,"){
-        if(fsockopen($ip,$puerto,$errno,$errstr,1))
-        {
-		print "<td bgcolor=green width=20>";
+function check($ip,$puerto) {
+        if($puerto != "") {
+                $fp = @fsockopen($ip, $puerto, $errno, $errstr, 1);
+                if ($fp) {
+                        print "<td bgcolor=green width=20>";
+                        fclose($fp);
+                }
         }
         else
         {
-		print "<td bgcolor=red width=20>";
+                print "<td bgcolor=red width=20>";
         }
-    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$ficheroEditado=$_REQUEST['fichero'];
-	$nombreEditado=$_REQUEST['nombre'];
-	$fp = fopen($ficheroEditado, 'w');
-	fwrite($fp, $nombreEditado);
-	fclose($fp);
+        $ficheroEditado=$_REQUEST['fichero'];
+        $nombreEditado=$_REQUEST['nombre'];
+        $fp = fopen($ficheroEditado, 'w');
+        fwrite($fp, $nombreEditado);
+        fclose($fp);
 }
 
-$fileList = glob('./_puertos/*');
+$fileList = glob('./_puertos/*'); 
 natsort($fileList);
 
 print("
@@ -34,57 +35,59 @@ table, th, td {
 
 //CABECERAS DE TABLA
 print ("<table>");
-print ("
-<tr>
+print ("<tr>
 <td><b>Fecha</td>
 <td><b>Activo</td>
 <td><b>Nombre</td>
 <td><b>IP</td>
 <td><b>Puertos</td>
+
 <td><b>Mac</td>
 ");
 
 foreach($fileList as $filename){
-	if(is_file($filename)){
-		$ip_array = explode ("./_puertos/", $filename);	//CAPTURO IPS DESCUBIERTAS POR SCRIPT BASH
-		$ip = $ip_array[1];
-		print ("<tr>");
+        if(is_file($filename)){
+                $ip_array = explode ("./_puertos/", $filename); //PUERTOS
+                $ip = $ip_array[1];
+                print ("<tr>");
 
-                $ruta_last = "./_fecha/".$ip;			//FECHA
-                $last = fopen($ruta_last,"r");
-                print ("<td valign='top'>".fgets($last)."</td>");
-                fclose($ruta_last);
-                $linesP = fopen($filename,'r');
+                                $ruta_last = "./_fecha/".$ip;  //FECHA
+                                $last = fopen($ruta_last,"r");
+                                if ($last) {
+                                        print ("<td valign='top'>".fgets($last)."</td>");
+                                        fclose($last);
+                                }
+                                $linesP = fopen($filename,'r');
 
-		$puertos = (fgets($linesP));
-		$puerto = explode (",",$puertos);
-		$puerto = ($puerto[0]);
-		fclose($linesP);
-		check($ip,$puerto);				//COLOR SI ESTÁ ACTIVO EN ESTE MISMO MOMENTO
-		print ("</td>");
+                $puertos = (fgets($linesP));
+                $puerto = explode (",",$puertos);
+                $puerto = ($puerto[0]);
+                fclose($linesP);
+                check($ip,$puerto);                             //COLOR SI ESTÁ ACTIVO EN ESTE MISMO MOMENTO
+                print ("</td>");
 
-                $ruta_nombre = "./_nombre/".$ip;		//NOMBRE EDITABLE
-		touch($ruta_nombre);
+                $ruta_nombre = "./_nombre/".$ip;                //NOMBRE EDITABLE
+                touch($ruta_nombre);
                 $nombre = fopen($ruta_nombre,"r");
-		print ("<td style='font-size:0px;'><form method='post'><input type='hidden' name='fichero' value='".$ruta_nombre."'><input style='font-size:14px;border:0;outline:0;' type='text' name='nombre' value='".fgets($nombre)."'></form></td>");
-                fclose($ruta_nombre);
+                print ("<td style='font-size:0px;'><form method='post'><input type='hidden' name='fichero' value='".$ruta_nombre."'><input style='font-size:14px;border:0;outline:0;' type='te>
+                fclose($nombre);
 
-	        print ("<td valign='top'>".$ip."</td>");	//IP
+                print ("<td valign='top'>".$ip."</td>");        //IP
 
-		print("<td valign='top'>");			//LISTADO DE PUERTOS
-		$lines = fopen($filename,'r');
-		while ($line = fgets($lines)) {
-			print ($line);
-		}
-		fclose($lines);
-		print("</td>");
+                print("<td valign='top'>");                     //LISTADO DE PUERTOS
+                $lines = fopen($filename,'r');
+                while ($line = fgets($lines)) {
+                        print ($line);
+                }
+                fclose($lines);
+                print("</td>");
 
-                $ruta_arp = "./_mac/".$ip;			//MAC
+                $ruta_arp = "./_mac/".$ip;                      //MAC
                 $arp = fopen($ruta_arp,"r");
                 print ("<td valign='top'>".fgets($arp)."</td>");
-                fclose($ruta_arp);
-		print ("</tr>");
-	}
+                fclose($arp);
+                print ("</tr>");
+        }
 }
 print ("</table>");
 ?>
